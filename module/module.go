9859fd9b49e34cf3e6025dd30a69e79a17ac4ab3
vitalspace <myglobalsounds@gmail.com>
2023-03-26 22:03:30 -0600

    Init

---
+++ module/module.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Module represents a module with a private message
+type Module struct {
+	private string
+}
+
+// sayPrivateInternal prints the module's private message
+func (m Module) sayPrivateInternal() {
+	fmt.Println(m.private)
+}
+
+// sayPublic calls sayPrivateInternal to print the module's private message
+func (m Module) sayPublic() {
+	m.sayPrivateInternal()
+}
+
+func main() {
+	// Create a new module with a private message
+	m := Module{private: "Hello world this is a private message"}
+
+	// Call the module's sayPublic method to print the private message
+	m.sayPublic()
+}
---
+++ module/module_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"testing"
+)
+
+// TestSayPublic is a unit test for the sayPublic method
+func TestSayPublic(t *testing.T) {
+	// Create a new module with a private message
+	m := Module{private: "Hello world this is a private message"}
+
+	// Call the module's sayPublic method
+	m.sayPublic()
+
+	// There is no explicit assertion in this test, as the output of sayPublic is printed to the console
+	// However, if sayPublic is not functioning correctly, the test will fail.
+	// This test can be run using the "go test" command in the terminal.
+}
